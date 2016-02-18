2f4739280d2ab1cc1f8487a8bc9ec43017749df9
H3xed <marczynski.net@gmail.com>
2016-02-18 17:24:12 +0100

    Poland locale

---
+++ src/locale/pl_PL.js
@@ -0,0 +1,11 @@
+/*
+ * pl-pl locale
+ * @ignore
+ * @author marczynski.net@gmail.com
+ */
+module.exports = {
+  // in minutes
+  timezoneOffset: 1 * 60,
+  firstDayOfWeek: 1,
+  minimalDaysInFirstWeek: 1,
+};
