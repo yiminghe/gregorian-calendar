548ed92ed1ec827790f1dea8a40e9219b30cf14e
plandem <plandem@gmail.com>
2016-02-20 00:56:19 +0300

    ru locale

---
+++ src/locale/ru_RU.js
@@ -0,0 +1,11 @@
+/*
+ * ru-RU (Moscow) locale
+ * @ignore
+ * @author plandem@gmail.com
+ */
+module.exports = {
+	// in minutes
+	timezoneOffset: 3 * 60,
+	firstDayOfWeek: 1,
+	minimalDaysInFirstWeek: 1,
+};
