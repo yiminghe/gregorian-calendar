b9a6a6a175fa9107be864f8ad2e0fd1445ab66a3
yiminghe <yiminghe@gmail.com>
2016-03-16 19:33:36 +0800

    bump for locale

--- src/locale/fi_FI.js
+++ src/locale/fi_FI.js
@@ -3,11 +3,9 @@
  * @ignore
  * @author jari@warpenius.fi
  */
-"use strict";
-
 module.exports = {
   // in minutes
   timezoneOffset: 2 * 60,
   firstDayOfWeek: 1,
-  minimalDaysInFirstWeek: 1
+  minimalDaysInFirstWeek: 1,
 };
